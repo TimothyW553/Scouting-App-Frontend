97ea1d98abf0503681b95fcbbbee37862ea60f16
TimothyW553 <timothywang@crescentschool.org>
2020-02-01 16:26:18 -0500

    Update InMatch.js

--- src/components/form/InMatch.js
+++ src/components/form/InMatch.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
-import { createMatchForm } from '../../store/actions/matchFormActions';
+import { createMatchForm } from '../../store/actions/matchFormActions'
+import ReactStopwatch from 'react-stopwatch'
 import './style.css'
 
 const red_field = './red-field.jpg';
 const blue_field = './blue-field.jpg';
-
-let PLAYERS = [
-  {
-    name: "high",
-    score: 0,
-    id: 1  
-  },
-  {
-    name: "low",
-    score: 0,
-    id: 2
-  },
-  {
-    name: "miss",
-    score: 0,
-    id: 3
-  }
-];
 
 function Counter(props) {
   return (
@@ -35,16 +18,15 @@ function Counter(props) {
   );
 }
 
-function Player(props) {
+function Shot(props) {
   return (
-    <div className="player">
-      <div className="player-score">
+    <div className="shot">
+      <div className="shot-score">
         <Counter score={props.score} onChange={props.onScoreChange}/>
       </div>
     </div>
   );
 };
-
 
 class Form extends Component {
   constructor(props) {
@@ -54,9 +36,6 @@ class Form extends Component {
       cycle_time: [0.0],
       climb_time: 0.0,
       balls_scored: 0,
-      outer: 0,
-      lower: 0,
-      miss: 0,
       floor_pickup: false,
       station_pickup: false,
       defense_time: 0.0,
@@ -68,8 +47,24 @@ class Form extends Component {
       time: 0,
       isOn: false,
       start: 0,
-      inMatchView: false,
-      players: PLAYERS
+      inMatchView: 0,
+      shots:[
+        {
+          type: "high",
+          score: 0,
+          id: 1  
+        },
+        {
+          type: "low",
+          score: 0,
+          id: 2
+        },
+        {
+          type: "miss",
+          score: 0,
+          id: 3
+        }
+      ]
     }
     this.startTimer = this.startTimer.bind(this);
     this.stopTimer = this.stopTimer.bind(this);
@@ -77,7 +72,7 @@ class Form extends Component {
   }
 
   onScoreChange(index, delta) {
-    this.state.players[index].score += delta;
+    this.state.shots[index].score += delta;
     this.setState(this.state);
   }
   
@@ -101,9 +96,21 @@ class Form extends Component {
     this.setState({time: 0, isOn: false})
   }
 
+  showPreMatch = (e) => {
+    e.preventDefault();
+    this.setState({inMatchView: 1})
+    console.log(this.state);
+  }
+
   showInMatch = (e) => {
     e.preventDefault();
-    this.setState({inMatchView: true})
+    this.setState({inMatchView: 2, preloads: this.state.balls_scored})
+    console.log(this.state);
+  }
+
+  showEndMatch = (e) => {
+    e.preventDefault();
+    this.setState({inMatchView: 3})
     console.log(this.state);
   }
 
@@ -140,39 +147,69 @@ class Form extends Component {
     console.log(this.state)
   }
 
+  incrementPreload = (e) => {
+    e.preventDefault();
+    if(this.state.balls_scored + 1 > 3) {
+      this.setState({
+        balls_scored : 3
+      })
+    } else {
+      this.setState({
+        balls_scored: this.state.balls_scored + 1
+      })
+    }
+  }
+
+  resetPreload = (e) => {
+    e.preventDefault();
+    this.setState({
+      balls_scored: 0
+    })
+  }
+
   render() {
     const { auth } = this.props;
     if (!auth.uid) return <Redirect to='/signin' />
-    let newMatchForm = (!this.state.inMatchView) ?
-      <div className="card text-left">
-        <div className="card-header">
-          New Match
+    let newMatchForm = (this.state.inMatchView === 0) ?
+      <form className="white" onSubmit={this.showPreMatch}>
+        <div className="input-field">
+          <p style={{fontWeight: 'bold', fontSize: 25}}>Enter the current match number:</p>
+          <input type='number' id="match_num" onChange={this.handleChange} placeholder="Match number"/>
         </div>
-        <div className="card-body">
-          <form className="white" onSubmit={this.showInMatch}>
-            <div className="input-field">
-              <p style={{fontWeight: 'bold', fontSize: 25}}>Enter the current match number:</p>
-              <input type='number' id="match_num" onChange={this.handleChange} placeholder="Match number"/>
-            </div>
-            <div className="input-field">
-              <button className="btn pink lighten-1">Next</button>
-            </div>
-          </form>
+        <div className="input-field">
+          <button className="btn pink lighten-1">Next</button>
         </div>
-      </div> : null
+      </form> : null
 
-    let matchField = <img src={require(`${red_field}`)} width="728" height="450" onClick={this.clicky} id="clickyimg"></img>
+    let prematch = (this.state.inMatchView === 1) ? (
+      <form className="white" onSubmit={this.showInMatch}>
+        <div className="input-field">
+          <p style={{fontWeight: 'bold', fontSize: 25}}>Number of Preloads</p>
+          <button type='number' id="balls_scored" onClick={this.incrementPreload} className="preload increment">
+            Preloads: {this.state.balls_scored}
+            </button>
+          <button type='number' id="balls_scored" onClick={this.resetPreload} className="preload decrement">
+            Reset Preloads
+            </button>
+        </div>
+        <div className="input-field">
+          <button className="btn pink lighten-1" onSubmit={this.showInMatch}>Next</button>
+        </div>
+      </form>
+    ) : null
+
+    let matchField = <img src={require(`${red_field}`)} width="760" height="470" onClick={this.clicky} id="clickyimg"></img>
     
-    let inMatchForm = (this.state.inMatchView) ? matchField : null
+    let inMatchForm = (this.state.inMatchView === 2) ? matchField : null
 
-    let scoreboard = (this.state.inMatchView) ? (
+    let scoreboard = (this.state.inMatchView === 2) ? (
       <span className="scoreboard">
-        <span className="players">
-          {this.state.players.map(function (player, index) {
+        <span className="shots">
+          {this.state.shots.map(function (shot, index) {
             return (
-              <Player
+              <Shot
                 onScoreChange={function (delta) { this.onScoreChange(index, delta) }.bind(this)}
-                score={player.score} key = {index}
+                score={shot.score} key = {index}
                 />
             );
           }.bind(this))}
@@ -180,27 +217,28 @@ class Form extends Component {
       </span>
     ) : null
 
-    let field_input = (this.state.inMatchView) ? 
-      <div className="card text-left">
-        <div className="card-header">
-          In Match Form
-        </div>
-        <div className="card-body">
-          <table className="FieldInput">
+    let field_input = (this.state.inMatchView === 2) ? 
+      <div>
+        <table className="FieldInput">
+          <tbody>
             <tr>
-              <td>
-              {scoreboard}
-              </td>
-              <td>
-              {inMatchForm}
-              </td>
+              <td>{scoreboard}</td>
+              <td>{inMatchForm}</td>
             </tr>
-          </table>
+          </tbody>
+        </table> 
+        <div className="input-field">
+          <button className="btn pink lighten-1" onSubmit={this.showEndMatch}>Next</button>
         </div>
-      </div> : null
+      </div>: null
       
+    let endform = (this.state.inMatchView === 3) ? (
+      <div>
+
+      </div>
+    ) : null
     
-    let team_select = (this.state.inMatchView) ? (
+    let team_select = (this.state.inMatchView === 2) ? (
       <div className="card text-left">
         <div className="card-header">
           Team Select
@@ -216,13 +254,14 @@ class Form extends Component {
     let resume = (this.state.time !== 0 && !this.state.isOn) ? <button onClick={this.startTimer}>resume</button> : null
 
     return (
-      <div className="card text-center">
-        <div className="card-header" style={{fontWeight: 'bold'}}>
-          Scouting Match Form
+      <div>
+        <div>
+        {}
         </div>
-        <span className="card-body">
+        <span>
           {newMatchForm}
-          {team_select}
+          {prematch}
+          {/* {team_select} */}
           {field_input}
         </span>
       </div>
