e11b8c020178fc9bec6ee68acb14811e07307474
mars-xiang <58280136+mars-xiang@users.noreply.github.com>
2020-02-11 19:01:58 -0500

    Update SortTB.js

--- src/components/overall/SortTB.js
+++ src/components/overall/SortTB.js
@@ -3,31 +3,34 @@ import React, { Component } from "react";
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 import firebase from "../../config/fbConfig.js";
 
-let refresh = async function(asfunc, func2, that) {
-  await asfunc(that);
+let refresh = async function(asfunc, f1args, func2, f2args, that) {
+  await asfunc(that, f1args);
   that.setState({ refresh: !that.state.refresh });
-  func2(that);
+  func2(that, f2args);
   that.setState({ refresh: !that.state.refresh });
 };
 
-let getAvg = async that => {
+let getAvg = async (that, team_number) => {
   await firebase
     .firestore()
     .collection("match_forms")
     .get()
     .then(snapshot => {
-      let len = snapshot.docs.length;
       snapshot.docs.forEach(doc => {
-        let listcopy = [...that.state.balls_scored_avg];
-        listcopy.push(doc.data().balls_scored);
-        that.setState({ balls_scored_avg: listcopy });
-        console.log(listcopy);
+        if (doc.data().teamSelected == team_number) {
+          let listcopy = [...that.state.balls_scored_avg];
+          listcopy.push(doc.data().balls_scored);
+          that.setState({ balls_scored_avg: listcopy });
+        }
       });
+      let len = that.state.balls_scored_avg.length;
       let sum = 0;
       that.state.balls_scored_avg.forEach(value => {
         sum += value;
       });
-      that.setState({ balls_scored_avg: (sum / len).toFixed(3) });
+      that.setState({
+        balls_scored_avg: len ? (sum / len).toFixed(3) : "No data"
+      });
     });
 };
 
@@ -56,7 +59,7 @@ let fetchAndLog = async that => {
   for (let i = 0; i < json_temp.length; i++) {
     let jsoncopy = [...that.state.json];
     jsoncopy.push({
-      TeamNumber: json_temp[i].team_number,
+      TeamNumber: i ? json_temp[i].team_number : 6969,
       CycleTime: getRandomInt(100),
       CycleTime: getRandomInt(100),
       Upper: getRandomInt(100),
@@ -83,9 +86,34 @@ class SortTB extends Component {
     // refresh(getAvg, this);
   }
 
+  docs;
+
+  getDocs = async () => {
+    this.docs = await firebase
+      .firestore()
+      .collection("match_forms")
+      .get()
+      .then(snapshot => {
+        return snapshot;
+      });
+  };
+
+  getAvg = team_number => {
+    let count = 0;
+    let value = 0;
+    let team_num = this.docs.data().teamSelected;
+    this.docs.data().forEach(val => {
+      if (team_number == team_num) {
+        count++;
+        value += val.balls_scored_avg;
+      }
+    });
+    value /= count;
+  };
+
   componentDidMount() {
     // fetchAndLog(this);
-    refresh(getAvg, setAvg, this);
+    refresh(getAvg, 6969, setAvg, null, this);
   }
 
   render() {
