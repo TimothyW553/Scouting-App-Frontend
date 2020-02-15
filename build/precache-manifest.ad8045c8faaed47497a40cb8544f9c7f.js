b7dcf13e7599aaef3a3d2b4be3bc522174b0c154
TimothyW553 <timothywang@crescentschool.org>
2020-02-15 17:12:21 -0500

    32

---
+++ build/precache-manifest.ad8045c8faaed47497a40cb8544f9c7f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60d99b969125aa66b5d88069bc0a90f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e9b2d9ccf3ce06c990",
+    "url": "/static/css/2.b1f7b954.chunk.css"
+  },
+  {
+    "revision": "07df7921be94d09ed978",
+    "url": "/static/css/main.245f036f.chunk.css"
+  },
+  {
+    "revision": "79e9b2d9ccf3ce06c990",
+    "url": "/static/js/2.51f35646.chunk.js"
+  },
+  {
+    "revision": "a78ecb6cd7847e6a86c687f27f8056ca",
+    "url": "/static/js/2.51f35646.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07df7921be94d09ed978",
+    "url": "/static/js/main.68e9bc9b.chunk.js"
+  },
+  {
+    "revision": "c80bae48cdf384b85e9c",
+    "url": "/static/js/runtime-main.7551f220.js"
+  },
+  {
+    "revision": "66add5faad3ff144d83ecca94269bead",
+    "url": "/static/media/blue-field.66add5fa.jpg"
+  },
+  {
+    "revision": "b1cfca0d1c0dfed7644632d2ba8653a2",
+    "url": "/static/media/circle.b1cfca0d.png"
+  },
+  {
+    "revision": "01b97d6200edb468865a4134267c9287",
+    "url": "/static/media/red-field.01b97d62.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ed2b55ad1b018cfba9acb1e0bf557ad.js"
+  "/precache-manifest.ad8045c8faaed47497a40cb8544f9c7f.js"
 );
 
 self.addEventListener('message', (event) => {
